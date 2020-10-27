e49484c580b23fe23a2687fab160449cf90075ed
rohanToss <rohantestrocky94@gmail.com>
2020-10-27 16:09:13 +0530

    demo complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  reason;
+  amount;
+  expenses =[];
+  totalExpense=0.0;
 
   constructor() {}
+  onSubmitExpense(expense){
+    console.log('expense reason: ',expense.reason);
+    console.log('expense amount: ',expense.amount);
+    if(expense.amount && expense.reason){
+      this.expenses.push({reason:this.reason,amount:this.amount});
+     this.totalExpense+= this.amount; 
+     this.onClear()
+    }
+  }
+  onClear(){
+    this.reason = '';
+    this.amount = '';
+  }
 
 }
